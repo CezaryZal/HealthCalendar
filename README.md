41ce834650202ad010ef55e3a37d963fc8869599
CezaryZal <czarek0821@gmail.com>
2019-11-14 23:28:15 +0100

    migration project to Spring boot with DATA JPA

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # HealthCalendar
+
+## Applied technologies
+Backend:
+- Maven
+- Spring Boot
+- JPA API
+- REST API
+- server application Tomcat
+
+## To do
+- validation input
+- add put method
+- change action(repository) to JPA method
--- src/main/java/com/CezaryZal/Application.java
+++ src/main/java/com/CezaryZal/Application.java
@@ -2,10 +2,8 @@ package com.CezaryZal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-//@ImportResource("config/persistence.xml")
 
 public class Application {
     public static void main(String[] args) {
