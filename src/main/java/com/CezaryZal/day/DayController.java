5b48bd364d24ffa0b2d3b0e1fc5fb9ae87e22a64
CezaryZal <czarek0821@gmail.com>
2019-11-17 20:41:38 +0100

    add annotation for controller to use CORS

--- src/main/java/com/CezaryZal/day/DayController.java
+++ src/main/java/com/CezaryZal/day/DayController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/day")
 public class DayController {
