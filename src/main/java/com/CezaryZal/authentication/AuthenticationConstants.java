f99f51a5fb9c1e41993e2ac092035ac78d2a7772
CezaryZal <czarek0821@gmail.com>
2019-12-23 11:39:48 +0100

    Create `configurationData.properties` and connection with project
    Creat to contain secret key and other data

--- src/main/java/com/CezaryZal/authentication/AuthenticationConstants.java
+++ src/main/java/com/CezaryZal/authentication/AuthenticationConstants.java
@@ -1,33 +1,22 @@
 package com.CezaryZal.authentication;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthenticationConstants {
 
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String TOKEN_TYPE = "JWT";
-    public static final String TOKEN_ISSUER = "secure-api";
-    public static final String TOKEN_AUDIENCE = "secure-app";
     public static final String TOKEN_HEADER = "Authorization";
     public static final String ROLES_KEY = "roles";
     public static final String NAME_KEY = "name";
-//    public static String SECRET_KEY;
-    public static String SECRET_KEY = "dFKYX}&[K;'{{;q";
-    public static Long JWT_EXPIRE_IN_MINUTES;
-    public static Long REFRESH_TOKEN_EXPIRE_IN_MINUTES;
+    public static String SECRET_KEY;
+
     public AuthenticationConstants() {
     }
-//    @Value("${authorization.jwt.token.expire.time.in.minutes}")
-//    private void setJwtExpireInMinutes(Long jwtExpireInMinutes) {
-//        JWT_EXPIRE_IN_MINUTES = jwtExpireInMinutes;
-//    }
-//    @Value("${authorization.refresh.token.expire.time.in.minutes}")
-//    private void setRefreshTokenExpireInMinutes(Long refreshTokenExpireInMinutes) {
-//        REFRESH_TOKEN_EXPIRE_IN_MINUTES = refreshTokenExpireInMinutes;
-//    }
-//    @Value("${authorization.jwt.secretkey}")
-//    private void setSecretKey(String secretKey) {
-//        this.SECRET_KEY = secretKey;
-//    }
+
+    @Value("${authorization.jwt.secret.key}")
+    private void setSecretKey(String secretKey) {
+        SECRET_KEY = secretKey;
+    }
 }
