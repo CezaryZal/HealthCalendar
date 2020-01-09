da8e455e93bf0e7a7cfb71c097b26f0a99ed4898
CezaryZal <czarek0821@gmail.com>
2020-01-09 12:37:29 +0100

    Prepare for application connection
    Add endpoint in `TestController` class;
    Remove `TestEntity` class;

--- src/main/java/com/CezaryZal/testDB/TestController.java
+++ src/main/java/com/CezaryZal/testDB/TestController.java
@@ -1,8 +1,9 @@
 package com.CezaryZal.testDB;
 
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+import com.CezaryZal.api.user.entity.UserCreator;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,16 +18,21 @@ public class TestController {
 
 
     @GetMapping("/user")
-    public TestEntity tmpEntity(){
-        TestEntity entity = new TestEntity("Dziad", 1234);
+    public UserCreator getUserCreator(){
+        UserCreator userCreator = new UserCreator("Dziad", "przypadkowy@gg.com");
 
-        return entity;
+        return userCreator;
     }
 
-    @GetMapping("/authe")
-    public String showAuthentication(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        return authentication.toString();
+    @PostMapping("/get/login")
+    public String getLoginByUserCreator(@RequestBody UserCreator userCreator){
+        return userCreator.getLoginName();
     }
+
+//    @GetMapping("/authe")
+//    public String showAuthentication(){
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        return authentication.toString();
+//    }
 
 }
