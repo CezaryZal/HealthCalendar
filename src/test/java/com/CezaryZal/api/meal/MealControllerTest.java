d1d46b669ce2368e0c403b008af75642f6fd58b8
CezaryZal <czarek0821@gmail.com>
2020-04-10 17:34:54 +0200

    Create MealControllerTest class with test to first GET method

---
+++ src/test/java/com/CezaryZal/api/meal/MealControllerTest.java
@@ -0,0 +1,42 @@
+package com.CezaryZal.api.meal;
+
+import com.CezaryZal.api.meal.manager.MealService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import javax.annotation.Resource;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(MealController.class)
+public class MealControllerTest {
+
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    public void ShouldReturnOkStatusFromGetDailyDietByDayIdMethod() throws Exception {
+
+        mockMvc.perform(get("/api/meal/dto/day-id/{dayId}", 1L))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
