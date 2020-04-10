49f5f9dc1c6d5ccf962bf33100f5623e463bfeec
CezaryZal <czarek0821@gmail.com>
2020-04-10 22:08:01 +0200

    Create test to incorrect dateTimeOfEat in POST method

--- src/test/java/com/CezaryZal/api/meal/MealControllerTest.java
+++ src/test/java/com/CezaryZal/api/meal/MealControllerTest.java
@@ -19,9 +19,10 @@ import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(MealController.class)
@@ -33,8 +34,11 @@ public class MealControllerTest {
     @Resource
     private WebApplicationContext webApplicationContext;
 
+    @MockBean
+    private MealService mealService;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .build();
         dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH:mm");
@@ -51,7 +55,7 @@ public class MealControllerTest {
     @Test
     public void ShouldReturnCreateStatusFromAddMealMethod() throws Exception {
 
-        String mealDtoInJson = "{\"dateTimeOfEat\": \"" + dateFormat.format(new Date()) +"\"," +
+        String mealDtoInJson = "{\"dateTimeOfEat\": \"" + dateFormat.format(new Date()) + "\"," +
                 " \"dayId\": 9," +
                 " \"description\": \"jaja1\"," +
                 " \"kcal\": 80," +
@@ -67,7 +71,7 @@ public class MealControllerTest {
     @Test
     public void ShouldReturnCreateStatusFromUpdateMealMethod() throws Exception {
 
-        String mealDtoInJson = "{\"dateTimeOfEat\": \"" + dateFormat.format(new Date()) +"\"," +
+        String mealDtoInJson = "{\"dateTimeOfEat\": \"" + dateFormat.format(new Date()) + "\"," +
                 " \"dayId\": 9," +
                 " \"description\": \"jaja2\"," +
                 " \"kcal\": 200," +
@@ -86,6 +90,39 @@ public class MealControllerTest {
         mockMvc.perform(delete("/api/meal/{id}/{userId}", 1L, 1L))
                 .andDo(print())
                 .andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void checkIncorrectDateTimeOfEatInPostMethod() throws Exception {
+
+        String mealDtoInJson = "{\"dateTimeOfEat\": \"2020-04-08_10:10\",\n" +
+                " \"dayId\": 9,\n" +
+                " \"description\": \"kanapki\",\n" +
+                " \"kcal\": 600,\n" +
+                " \"type\": \"obiad\"}";
+
+        mockMvc.perform(post("/api/meal/current/{userId}", 1L)
+                .content(mealDtoInJson)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.timestamp", is(notNullValue())))
+                .andExpect(jsonPath("$.path", is("uri=/api/meal/current/1")))
+                .andExpect(jsonPath("$.httpMethod", is("POST")))
+                .andExpect(jsonPath("$.status", is("BAD_REQUEST")))
+                .andExpect(jsonPath("$.message",
+                        is("Validation failed for argument [0] in public org.springframework.http." +
+                                "ResponseEntity<java.lang.String> com.CezaryZal.api.meal.MealController." +
+                                "addMeal(com.CezaryZal.api.meal.model.MealDto,java.lang.Long): [Field error " +
+                                "in object 'mealDto' on field 'dateTimeOfEat': rejected value " +
+                                "[2020-04-08T10:10]; codes [ActualDate.mealDto.dateTimeOfEat,ActualDate." +
+                                "dateTimeOfEat,ActualDate.java.time.LocalDateTime,ActualDate]; arguments " +
+                                "[org.springframework.context.support.DefaultMessageSourceResolvable: codes " +
+                                "[mealDto.dateTimeOfEat,dateTimeOfEat]; arguments []; default message " +
+                                "[dateTimeOfEat]]; default message [The date time should be current]] ")))
+                .andExpect(jsonPath("$.errors").isArray())
+                .andExpect(jsonPath("$.errors", hasSize(1)))
+                .andExpect(jsonPath("$.errors", hasItem("The date time should be current")));
     }
 
     public static String asJsonString(final Object obj) {
